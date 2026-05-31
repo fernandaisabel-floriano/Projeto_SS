206bc49bab047ae7401281d7e2d47744b85974cd
felipesilvarocha <felipe.rocha@ua.pt>
2026-05-31 09:58:32 +0100

    Add files via upload

--- stateMachine.c
+++ stateMachine.c
@@ -17,6 +17,7 @@ static States actualState = ST0_WAIT;
 
 volatile int num_lidos = 0;
 volatile int sequencia[4];
+volatile int estado_led = 0;
 static const int PASSWORD_OPEN[4] = {1, 0, 1, 2};
 static const int PASSWORD_FECHAR[4] = {0, 0, 1, 2};
 
@@ -25,26 +26,19 @@ gptimer_handle_t T1 = NULL;
 gptimer_handle_t T2 = NULL;
 
 static bool timer_isr_callback1(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data) {
+    
+    estado_led = !estado_led;
+    gpio_set_level(LED, estado_led);
 
-    
-    /*liga e desliga o led*/
-    
     return false;
 };
 static bool timer_isr_callback2(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data) {
     gptimer_stop(T2);
     gptimer_stop(T1);
-    actualState = ST0_WAIT;
     return false;
 }
 
-
-
-void main_estados(){
-
-    gptimer_handle_t T1 = NULL;
-    gptimer_handle_t T2 = NULL;
-
+void timerConfig(){
     gptimer_config_t Timer_config = {
     .clk_src = GPTIMER_CLK_SRC_DEFAULT, // Select the default clock source
     .direction = GPTIMER_COUNT_UP,      // Counting direction is up
@@ -81,51 +75,90 @@ void main_estados(){
 
     ESP_ERROR_CHECK(gptimer_enable(T1));
     ESP_ERROR_CHECK(gptimer_enable(T2));
-    
+}
+
+void ledconfig(){
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+}
+
+void main_estados(){
+
+    timerConfig();
+    ledconfig();
+
+    int cmp[] = {0,0};
+
 while(1){    
     switch (actualState){
 
         case ST0_WAIT:
-
             while(num_lidos <= 3){
                 if (volume1 > 1000)
                 {
                     sequencia[num_lidos] = 0;
                     num_lidos++;
-                    vTaskDelay(20000 / portTICK_PERIOD_MS);
+                    vTaskDelay(100 / portTICK_PERIOD_MS);
                 }
                 else if (volume2 > 1000)
                 {
                     sequencia[num_lidos] = 1;
                     num_lidos++;
-                    vTaskDelay(20000 / portTICK_PERIOD_MS);
+                    vTaskDelay(100 / portTICK_PERIOD_MS);
                 }
                 else if (volume3 > 1000){
                     sequencia[num_lidos] = 2;
                     num_lidos++;
-                    vTaskDelay(20000 / portTICK_PERIOD_MS);
+                    vTaskDelay(100 / portTICK_PERIOD_MS);
                 }
             }
-
-            memset((void*)sequencia, 0, sizeof(sequencia));
-            num_lidos = 0;
             actualState = ST1_VALIDATE;
+            break;
 
         case ST1_VALIDATE:
-            /*checka a sequencia troca de estado*/
+            cmp[0] = memcmp(sequencia,PASSWORD_OPEN,sizeof(sequencia));
+            cmp[1] = memcmp(sequencia,PASSWORD_FECHAR,sizeof(sequencia));
+            if (cmp[0] == 0)
+            {
+                actualState = ST2_OPEN;
+            }
+            else if (cmp[1] == 0)
+            {
+                actualState = ST3_CLOSE;
+            }
+            else
+            {
+                actualState = ST4_ERR;
+            }
+            memset((void*)sequencia, 0, sizeof(sequencia));
+            cmp[0] = 0;
+            cmp[1] = 0;
+            num_lidos = 0;
+            break;
+
         case ST2_OPEN:
-        /*liga o led*/
+            estado_led = 1;
+            gpio_set_level(LED, estado_led);
+            actualState = ST0_WAIT;
+            break;
+
         case ST3_CLOSE:
-        /* desliga o led */
+            estado_led = 0;
+            gpio_set_level(LED,estado_led);
+            actualState = ST0_WAIT;
+            break;
+
         case ST4_ERR:
             ESP_ERROR_CHECK(gptimer_start(T1));
             ESP_ERROR_CHECK(gptimer_start(T2));
-            vTaskDelay(20000 / portTICK_PERIOD_MS);
-            
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            actualState = ST0_WAIT;
+            break;
+
         default:
             break;
         }
-        vTaskDelay(20000 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
 
